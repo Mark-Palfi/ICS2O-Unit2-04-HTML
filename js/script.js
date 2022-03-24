d5cc8fb1be2803b2645bac4349a90a008b422938
Mark-Palfi <99292598+Mark-Palfi@users.noreply.github.com>
2022-03-24 20:09:20 +0000

    change rectangle to triangle

--- js/script.js
+++ js/script.js
@@ -4,16 +4,18 @@
 
 'use strict'
 /**
- * This function calculates area and perimeter of rectangle.
+ * This function calculates area and perimeter of triangle.
  */
 function calculate () {
   // input
-  const length = parseInt(document.getElementById('length-of-rectangle').value)
-  const width = parseInt(document.getElementById('width-of-rectangle').value)
+  const A = parseInt(document.getElementById('length1-of-triangle').value)
+  const B = parseInt(document.getElementById('length2-of-triangle').value)
+  const C = parseInt(document.getElementById('length3-of-triangle').value)
+  const height = parseInt(document.getElementById('height-of-triangle').value)
 
   // process
-  const area = length * width
-  const perimeter = 2 * (length + width)
+  const area = (B * height) / 2
+  const perimeter = A + B + C
 
   // output
   document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
